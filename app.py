7d7b82da6076964308a879cc7006ac5472dc0e0c
Benjamin Crom <benjamin.crom@careerbuilder.com>
2015-08-13 18:33:23 -0400

    Added twitter api wrapper module

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+TWITTER_KEY = 'hhoGSNh6JkursHppu74AGnK5r:i7lLGpPSX1KESNNiAJgikHGuUsrZZaR0dRF9SvSTECxon7aPX6'
+TWITTER_KEY_BASE64 = TWITTER_KEY.encode('base64')
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+def twitter_bearer_token():
+
+
 
 if __name__ == '__main__':
     app.run()
