fe9d08c9e9c0256dc178bb96d47251e162447908
Benjamin Crom <benjamin.crom@careerbuilder.com>
2015-08-14 01:58:27 -0400

    added more functionality

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 import wikipedia
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from twitter_api_wrapper import TwitterAPIWrapper
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    twitter_api = TwitterAPIWrapper()
-    tweet_dict_list = twitter_api.get_topic('football')
-
-    wikipedia_list = wikipedia.search('football', results=15)
+    tweet_dict_list = []
     wikipedia_dict_list = []
-    for entry in wikipedia_list:
-        wikipedia_dict_list.append(
-            {
-                'title': entry,
-                'summary': wikipedia.summary(entry, sentences=1),
-            }
-        )
+
+    if 'search' in request.args:
+        search_string = request.args.get('search')
+
+        twitter_api = TwitterAPIWrapper()
+        tweet_dict_list = twitter_api.get_topic(search_string)
+
+        wikipedia_list = wikipedia.search(search_string, results=20)
+        wikipedia_dict_list = []
+        for entry in wikipedia_list:
+            try:
+                summary = wikipedia.summary(entry, sentences=1)
+            except wikipedia.exceptions.DisambiguationError as e:
+                summary = str(e.options[0])
+
+            wikipedia_dict_list.append(
+                {
+                    'title': entry,
+                    'summary': summary,
+                }
+            )
+
     return render_template(
         'index.html',
         tweet_dict_list=tweet_dict_list,
