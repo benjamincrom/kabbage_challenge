747cc7eecc00aa2095575ed04b2be2b316f777c9
Benjamin Crom <benjamin.crom@careerbuilder.com>
2015-08-14 11:06:07 -0400

    cleanup docstrings

--- app.py
+++ app.py
@@ -7,15 +7,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """ Return search page for tweets and wikipedia entries by keyword. """
     tweet_dict_list = []
     wikipedia_dict_list = []
 
     if 'search' in request.args:
         search_string = request.args.get('search')
-
-        twitter_api = TwitterAPIWrapper()
-        tweet_dict_list = twitter_api.get_topic(search_string)
-
+        twitter_api_obj = TwitterAPIWrapper()
+        tweet_dict_list = twitter_api_obj.get_topic(search_string)
         wikipedia_dict_list = wikipedia_api_wrapper.get_topic(search_string)
 
     return render_template('index.html',
