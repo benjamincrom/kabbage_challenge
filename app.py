633d0a7cf786f80c7b5cb47d3e34da7242b00a89
Benjamin Crom <benjamin.crom@careerbuilder.com>
2015-08-13 20:01:53 -0400

    Clean up app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-TWITTER_KEY = 'hhoGSNh6JkursHppu74AGnK5r:i7lLGpPSX1KESNNiAJgikHGuUsrZZaR0dRF9SvSTECxon7aPX6'
-TWITTER_KEY_BASE64 = TWITTER_KEY.encode('base64')
-
 @app.route('/')
 def hello():
     return "Hello World!"
